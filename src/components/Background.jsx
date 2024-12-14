4f0d9bc00833a2d0619ecd884011a3b0922bf14a
abhay25v <abhay25v@gmail.com>
2024-12-14 23:51:59 +0530

    maath

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,7 +1,7 @@
 import { useState, useRef, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
-import * as random from "maath/random/dist/math-random.esm";
+import * as random from "maath/random/dist/maath-random.esm.js";
 
 const Stars = (props) => {
 	const ref = useRef();
